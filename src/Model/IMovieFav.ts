c4ce4c81a096007274a8c550620f44a4a2411ac5
Siddhartha Shekhar <84010535+Sid-803@users.noreply.github.com>
2023-03-18 23:45:50 +0530

    Add files via upload

---
+++ src/Model/IMovieFav.ts
@@ -0,0 +1,16 @@
+export default interface IMovieFav {
+    title:string,
+    year:string,
+    genres:string[],
+    rating:number[],
+    poster:string,
+    contentRating:string,
+    duration:string,
+    releaseDate:string,
+    averageRating:string,
+    originalTitle:string,
+    storyline:string,
+    actors:string[],
+    imdbRating:string,
+    posterurl:string
+}
